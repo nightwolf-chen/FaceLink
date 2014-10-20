b21908c5491811ff7ee00815c4ec110cd39fd038
nirvawolf <466202783@qq.com>
2014-10-20 11:27:56 +0800

    添加匹配逻辑和进度显示的代码

--- FaceLink/FaceLink/ViewControllers/FLPhotoMatchingController.h
+++ FaceLink/FaceLink/ViewControllers/FLPhotoMatchingController.h
@@ -8,10 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FLPhotoMatchingController : UIViewController
+@interface FLPhotoMatchingController : UIViewController<UIGestureRecognizerDelegate>
 
 @property (nonatomic,strong) UIImage *useImage;
 
 @property (weak, nonatomic) IBOutlet UIImageView *userImageView;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *zodiacLabel;
+@property (weak, nonatomic) IBOutlet UILabel *basicInfoLabel;
+@property (weak, nonatomic) IBOutlet UILabel *introductionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (weak, nonatomic) IBOutlet UIView *userInfoContainerView;
+
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UIButton *complainButton;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
 
 @end
--- FaceLink/FaceLink/ViewControllers/FLPhotoMatchingController.m
+++ FaceLink/FaceLink/ViewControllers/FLPhotoMatchingController.m
@@ -7,8 +7,21 @@
 //
 
 #import "FLPhotoMatchingController.h"
+#import "ProgressHUD.h"
+
+static NSString *const kMatchingText = @"正在寻找同时拍照的人...";
+static NSString *const kMatchingSuccessText = @"恭喜你们成功互换了照片";
+static NSString *const kComplainSuccessText = @"已经举报该用户，谢谢反馈";
+
+NS_ENUM(NSInteger, FLPhotoMatchingState){
+    FLPhotoMatchingStateMatching,
+    FLPhotoMatchingStateSuccess,
+    FLPhotoMatchingStateFailed
+};
 
 @interface FLPhotoMatchingController ()
+
+@property (nonatomic,assign) NSInteger matchingState;
 
 @end
 
@@ -29,28 +42,99 @@
     _userImageView.image = useImage;
     _userImageView.backgroundColor = [UIColor yellowColor];
 }
+- (IBAction)buttonClicked:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    _userImageView.bounds = CGRectMake(0, 0,320,320);
+    
+    [self p_setupUserImageView];
+    [self p_hideViews:YES];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self p_startMatching];
+    });
+}
+
+- (void)p_setupUserImageView
+{
     _userImageView.contentMode = UIViewContentModeScaleAspectFit;
+    _userImageView.userInteractionEnabled = YES;
     _userImageView.image = _useImage;
-    // Do any additional setup after loading the view from its nib.
+    
+    UISwipeGestureRecognizer *swipeRecognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(imageViewSwiped:)];
+    swipeRecognizer.delegate = self;
+    swipeRecognizer.direction = (UISwipeGestureRecognizerDirectionDown |
+                                 UISwipeGestureRecognizerDirectionUp |
+                                 UISwipeGestureRecognizerDirectionLeft |
+                                 UISwipeGestureRecognizerDirectionRight);
+    
+    [_userImageView addGestureRecognizer:swipeRecognizer];
+}
+
+- (void)imageViewSwiped:(UIGestureRecognizer *)recognizer
+{
+    if (_matchingState == FLPhotoMatchingStateSuccess) {
+        [self p_startMatching];
+    }
+}
+
+- (void)p_startMatching
+{
+    _userImageView.image = _useImage;
+    
+    self.matchingState = FLPhotoMatchingStateMatching;
+    _statusLabel.text = kMatchingText;
+    
+    [self p_hideViews:YES];
+
+    //show progress indicator
+    [ProgressHUD show:kMatchingText];
+    
+    void (^matchingSuccessBlock)(void) = ^{
+        
+        [UIView animateWithDuration:0.5 animations:^{
+            _userImageView.image = nil;
+            _statusLabel.text = @"";
+            self.view.layer.transform = CATransform3DMakeRotation(M_PI,0.0,2.0,0.0);
+        } completion:^(BOOL finished){
+            
+            self.view.layer.transform = CATransform3DMakeRotation(M_PI,0.0,0.0,0.0);
+            [self p_hideViews:NO];
+            _statusLabel.text = kMatchingSuccessText;
+            self.matchingState = FLPhotoMatchingStateSuccess;
+            
+            [ProgressHUD showSuccess:kMatchingSuccessText];
+        }];
+
+    };
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(),matchingSuccessBlock);
+}
+- (IBAction)comlainButtonClicked:(id)sender {
+    [ProgressHUD showSuccess:kComplainSuccessText];
+}
+- (IBAction)likeButtonClicked:(id)sender {
+    _likeButton.backgroundColor = [UIColor redColor];
+}
+
+- (void)p_hideViews:(BOOL)hide
+{
+    _userInfoContainerView.hidden = hide;
+    _likeButton.hidden = hide;
+    _complainButton.hidden = hide;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
